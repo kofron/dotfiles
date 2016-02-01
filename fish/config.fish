a8c659de5292e79cf235df493600350af2312737
Jared Kofron <jaredk@porch.com>
2016-02-01 15:00:33 -0700

    adding fish config

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+alias e "emacsclient -cnw"
+alias prod "kubectl --context=gke_porch-gcp_us-central1-b_production-green"
+alias qa "kubectl --context=gke_porch-gcp_us-central1-b_qa-green"
+alias dev "kubectl --context=gke_porch-gcp_us-central1-b_development-green"
+
+set PATH ~/google-cloud-sdk/bin $PATH
+
