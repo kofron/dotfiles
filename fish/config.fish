31bede12f5ae30039e918754cfe3579927a1e6a1
Jared Kofron <jared.kofron@gmail.com>
2018-09-12 16:35:12 -0700

    work on all files

--- fish/config.fish
+++
@@ -1,7 +0,0 @@
-alias e "emacsclient -cnw"
-alias prod "kubectl --context=gke_porch-gcp_us-central1-b_production-green"
-alias qa "kubectl --context=gke_porch-gcp_us-central1-b_qa-green"
-alias dev "kubectl --context=gke_porch-gcp_us-central1-b_development-green"
-
-set PATH ~/google-cloud-sdk/bin $PATH
-
--- setup_links.sh
+++ setup_links.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ln -s $(pwd)/ranger ~/.config/ranger
-ln -s $(pwd)/.zshrc ~/.zshrc
-ln -s $(pwd)/emacs/.emacs ~/.emacs
+ln -s $(pwd)/zsh/dotzshrc ~/.zshrc
+ln -s $(pwd)/emacs/dotemacs ~/.emacs
