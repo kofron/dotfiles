e3c87887775a413c04cb368be053b8e455572d12
Jared Kofron <jared@kofron.io>
2025-10-23 07:09:54 -0700

    more emacs config

--- config/doom/init.el
+++ config/doom/init.el
@@ -157,7 +157,7 @@
        ;;nim               ; python + lisp at the speed of c
        nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +journal)               ; organize your plain life in plain text
+       (org +journal +pretty)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
